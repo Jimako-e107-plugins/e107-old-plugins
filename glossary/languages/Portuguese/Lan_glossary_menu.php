cfde1c15e51130c78af6da02fbda20c3c6cb7379
rica-carv <rica-carv@users.noreply.github.com>
2017-09-22 22:22:13 +0100

    Add files via upload

---
+++ glossary/languages/Portuguese/Lan_glossary_menu.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Glossary by Shirka (www.shirka.org)
+ *
+ * A plugin for the e107 Website System (http://e107.org)
+ *
+ * ©Andre DUCLOS 2006
+ * http://www.shirka.org
+ * duclos@shirka.org
+ *
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://gnu.org).
+ *
+ * $Source: /home/e-smith/files/ibays/cvsroot/files/glossary/languages/English/Lan_glossary_menu.php,v $
+ * $Revision: 1.4 $
+ * $Date: 2006/06/12 22:37:06 $
+ * $Author: duclos $
+ */
+
+if (!defined('e107_INIT')) { exit; }
+
+define('LAN_GLOSSARY_BLMENU_01', "Glossary");
+define('LAN_GLOSSARY_BLMENU_02', "View words frontpage");
+define('LAN_GLOSSARY_BLMENU_03', "Submit a definition");
+define('LAN_GLOSSARY_BLMENU_04', "Navigator");
+define('LAN_GLOSSARY_BLMENU_05', "No definitions defined yet.");
+define('LAN_GLOSSARY_BLMENU_06', "Create a definition");
+
+?>
