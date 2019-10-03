27f254e5d426b436ffb69116d6fc596bc13cd976
Jimako <janamicek@gmail.com>
2019-10-03 16:06:13 +0200

    fixed admin menu

--- clan_listing/admin_menu.php
+++ clan_listing/admin_menu.php
@@ -30,12 +30,10 @@ $text1 = "
 
 $text1 .= "
                 </td>
-                </tr><td style='width:30%' class='header'>-</b>";
+                </tr> ";
 
 $text1 .= "
-        </td>
-    </tr>
-
+ 
 	<tr>
         <td style='width:30%' class='button'>";
         if (e_PAGE == "admin_config.php") {
@@ -48,12 +46,10 @@ $text1 .= "
 
 $text1 .= "
                 </td>
-                </tr><td style='width:30%' class='header'>-</b>";
+                </tr> ";
 
 $text1 .= "
-        </td>
-    </tr>
-
+ 
 	<tr>
         <td style='width:30%' class='button'>";
         if (e_PAGE == "admin_new_cat.php") {
@@ -66,14 +62,13 @@ $text1 .= "
 
 $text1 .= "
                 </td>
-                </tr><td style='width:30%' class='header'>-</b>";
+                </tr> ";
 
 
 
 
 $text1 .= "
-        </td>
-    </tr>
+ 
 	<tr>
         <td style='width:30%' class='button'>";
         if (e_PAGE == "admin_edit_cat.php") {
@@ -85,17 +80,10 @@ $text1 .= "
 		$text1 .= "
 		<a style='cursor:hand; text-decoration:none' href='admin_edit_cat.php'> ".ACLANLIST_MENU_04." </a>";
 		}
-
-
 $text1 .= "
                 </td>
-                </tr><td style='width:30%' class='header'>-</b>";
-
-
-
+                </tr>";
 $text1 .= "
-        </td>
-    </tr>
 	<tr>
         <td style='width:30%' class='button'>";
         if (e_PAGE == "admin_edit_cat_order.php") {
@@ -111,13 +99,11 @@ $text1 .= "
 
 $text1 .= "
                 </td>
-                </tr><td style='width:30%' class='header'>-</b>";
+                </tr>";
 
 
 
 $text1 .= "
-        </td>
-    </tr>
 
 	<tr>
         <td style='width:30%' class='button'>";
@@ -134,12 +120,10 @@ $text1 .= "
 
 $text1 .= "
                 </td>
-                </tr><td style='width:30%' class='header'>-</b>";
+                </tr>";
 
 
 $text1 .= "
-        </td>
-    </tr>
 	<tr>
         <td style='width:30%' class='button'>";
         if (e_PAGE == "admin_edit.php") {
@@ -158,12 +142,11 @@ $text1 .= "
 
 $text1 .= "
                 </td>
-                </tr><td style='width:30%' class='header'>-</b>";
+                </tr> ";
 
 
 $text1 .= "
-        </td>
-    </tr>
+         
 	<tr>
         <td style='width:30%' class='button'>";
         if (e_PAGE == "admin_clan_submissions.php") {
@@ -179,74 +162,18 @@ $text1 .= "
 
 $text1 .= "
                 </td>
-                </tr><td style='width:30%' class='header'>-</b>";
-
-
-$text1 .= "
-        </td>
-    </tr>
-	<tr>
-        <td style='width:30%' class='button'>";
-        if (e_PAGE == "admin_vupdate.php") {
-         $text1 .= "
-		<b>
-		<a style='cursor:hand; text-decoration:none' href='admin_vupdate.php'>>> ".ACLANLIST_MENU_08." <<</a>
-		</b>";
-		} else {
-		$text1 .= "
-		<a style='cursor:hand; text-decoration:none' href='admin_vupdate.php'>".ACLANLIST_MENU_08."</a>";
-		}
-
-
-
-$text1 .= "
-                </td>
                 </tr>";
+
+
+
+$text1 .= "</table>";
 
 
 
 
 
 //--------------------------------------------------------------------------------------------
-
-
-$text1 .= "<tr><td><br><br><br><br>
-<center>
-Donate To AACGC.
-<form action='https://www.paypal.com/cgi-bin/webscr' method='post'>
-<input type='hidden' name='cmd' value='_s-xclick'>
-<input type='hidden' name='hosted_button_id' value='6506985'>
-<input type='image' src='https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif' border='0' name='submit' alt='PayPal - The safer, easier way to pay online!'>
-<img alt='' border='0' src='https://www.paypal.com/en_US/i/scr/pixel.gif' width='1' height='1'>
-</form>
-<br><br>
-</td></tr>";
-
-$text1 .= "<tr>
-           <td style='width:30%' class='button'>
-           <b><a style='cursor:hand; text-decoration:none' href='http://www.aacgc.com/SSGC/e107_plugins/faq/faq.php' target='_blank'>AACGC FAQs</a></b>
-           </td>
-           </tr>";
-
-
-$text1 .= "<tr><td style='width:30%' class='header'>-</b></td></tr>";
-
-
-$text1 .= "<tr>
-           <td style='width:30%' class='button'>
-           <b><a style='cursor:hand; text-decoration:none' href='http://www.aacgc.com/SSGC/e107_plugins/helpdesk3_menu/helpdesk.php' target='_blank'>AACGC HelpDesk</a></b>
-           </td>
-           </tr>";
-
-
-$text1 .= "<tr><td style='width:30%' class='header'>-</b></td></tr>";
-
-
-$text1 .= "<tr>
-           <td style='width:30%' class='button'>
-           <b><a style='cursor:hand; text-decoration:none' href='http://wiki.aacgc.com/index.php?title=Main_Page' target='_blank'>AACGC Wiki</a></b>
-           </td>
-           </tr>";
+ 
 
 //-----------------------------------------------------------------------------------------------------
 
