fb13ea11f7a2b6ed17e73e5196287391453bc8f3
Jimmi08 <janamicek@gmail.com>
2020-01-15 18:55:05 +0100

    trackback plugin

---
+++ trackback/languages/English.php
@@ -0,0 +1,37 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     Copyright (C) 2001-2002 Steve Dunstan (jalist@e107.org)
+|     Copyright (C) 2008-2010 e107 Inc (e107.org)
+|
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $URL: https://e107.svn.sourceforge.net/svnroot/e107/trunk/e107_0.7/e107_plugins/trackback/languages/English.php $
+|     $Revision: 11678 $
+|     $Id: English.php 11678 2010-08-22 00:43:45Z e107coders $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
+	
+define("TRACKBACK_L1", "Configure Trackback");
+define("TRACKBACK_L2", "This plugin enables you to use trackback in your news posts.");
+define("TRACKBACK_L3", "Trackback is now installed and enabled.");
+define("TRACKBACK_L4", "Trackback settings saved.");
+define("TRACKBACK_L5", "On");
+define("TRACKBACK_L6", "Off");
+define("TRACKBACK_L7", "Activate trackback");
+define("TRACKBACK_L8", "Trackback URL text");
+define("TRACKBACK_L9", "Save Settings");
+define("TRACKBACK_L10", "Trackback Settings");
+define("TRACKBACK_L11", "Trackback address for this post:");
+
+define("TRACKBACK_L12", "No trackbacks for this item");
+define("TRACKBACK_L13", "Moderate trackbacks");
+define("TRACKBACK_L14", "Delete");
+define("TRACKBACK_L15", "Trackbacks deleted.");
+
+?>
---
+++ trackback/languages/Slovak.php
@@ -0,0 +1,37 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvsroot/e107/e107_langpacks/e107_plugins/trackback/languages/Slovak.php,v $
+|     $Revision: 1.1 $
+|     $Date: 2005/07/15 20:58:15 $
+|     $Author: manro $
++----------------------------------------------------------------------------+
+*/
+	
+define("TRACKBACK_L1", "Nastav sledovanie");
+define("TRACKBACK_L2", "Tento plugin vám umožnuje nastavit sledovanie vo vašich novinkách.");
+define("TRACKBACK_L3", "Sledovanie je teraz inštalované a umožnené.");
+define("TRACKBACK_L4", "Nastavenia sledovania sú uložené.");
+define("TRACKBACK_L5", "Zapnuté");
+define("TRACKBACK_L6", "Vypnuté");
+define("TRACKBACK_L7", "Aktivuj sledovanie");
+define("TRACKBACK_L8", "Sledovanie URL textu");
+define("TRACKBACK_L9", "Ulož nastavenia");
+define("TRACKBACK_L10", "Nastavenia sledovania");
+define("TRACKBACK_L11", "Adresa sledovania pre túto tému:");
+
+define("TRACKBACK_L12", "Nie sú sledovania pre túto položku");
+define("TRACKBACK_L13", "Priemerné sledovania");
+define("TRACKBACK_L14", "Zmazat");
+define("TRACKBACK_L15", "Sledovania zmazané.");
+
+?>
