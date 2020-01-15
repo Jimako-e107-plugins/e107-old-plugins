010ec6cd43bff57bdbceaef697137cdf53d1def3
Jimmi08 <janamicek@gmail.com>
2020-01-15 18:54:21 +0100

    tree menu plugin

---
+++ tree_menu/languages/English.php
@@ -0,0 +1,30 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     Copyright (C) 2001-2002 Steve Dunstan (jalist@e107.org)
+|     Copyright (C) 2008-2010 e107 Inc (e107.org)
+|
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $URL: https://e107.svn.sourceforge.net/svnroot/e107/trunk/e107_0.7/e107_plugins/tree_menu/languages/English.php $
+|     $Revision: 11678 $
+|     $Id: English.php 11678 2010-08-22 00:43:45Z e107coders $
+|     $Author: e107coders $
++----------------------------------------------------------------------------+
+*/
+
+define("TREE_L1", "Configure Tree Menu");
+define("TREE_L2", "Update Tree Menu Settings");
+define("TREE_L3", "Tree Menu configuration saved.");
+define("TREE_L4", "On");
+define("TREE_L5", "Off");
+define("TREE_L6", "CSS class to use for non-openable links");
+define("TREE_L7", "CSS class to use for openable links");
+define("TREE_L8", "CSS class to use for opened links");
+define("TREE_L9", "Use spacer class between main links");
+
+?>
---
+++ tree_menu/languages/Slovak.php
@@ -0,0 +1,30 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system
+|
+|     ©Steve Dunstan 2001-2002
+|     http://e107.org
+|     jalist@e107.org
+|
+|     Released under the terms and conditions of the
+|     GNU General Public License (http://gnu.org).
+|
+|     $Source: /cvsroot/e107/e107_0.7/e107_plugins/tree_menu/languages/English.php,v $
+|     $Revision: 1.3 $
+|     $Date: 2005/10/07 02:54:20 $
+|     $Author: sweetas $
++----------------------------------------------------------------------------+
+*/
+
+define("TREE_L1", "Nastaviť stromové menu");
+define("TREE_L2", "Aktualizovať nastavenia stromového menu");
+define("TREE_L3", "Nastavenie stromového menu je uložené.");
+define("TREE_L4", "Zap");
+define("TREE_L5", "Vyp");
+define("TREE_L6", "CSS trieda k použitiu pre neotvoriteľné linky");
+define("TREE_L7", "CSS trieda k použitiu pre otvoriteľné linky");
+define("TREE_L8", "CSS trieda k použitiu pre otvorené linky");
+define("TREE_L9", "Použiť dištančnú triedu medzi hlavnými linkami");
+
+?>
